7e3354b5a63dfe57a51257a55e781f4a7d10840b
Hayato Kakiuchi <qweaty.uiop8810@i.softbank.jp>
2020-05-31 15:27:06 +0900

    add files, implement top

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
 
 <main>
+  <img src="<?php echo get_template_directory_uri(); ?>/images/IMG_2310.JPG" id ="top">
+  <div class="main-wrapper">
+  </div>
 </main>
 
 <?php get_footer(); ?>
---
+++ page-product.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<main>
+  <div class="main-wrapper">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/IMG_2310.JPG" class="image-responsive">
+    <h2 class="text-center">取り扱い商品</h2>
+    <p>ほげ</p>
+  </div>
+</main>
+
+<?php get_footer(); ?>
