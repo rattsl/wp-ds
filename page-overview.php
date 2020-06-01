9c0ef09ff8a6a567b512b140c49c381c8a2bc05f
Hayato Kakiuchi <qweaty.uiop8810@i.softbank.jp>
2020-06-01 20:42:22 +0900

    implement GoogleMao

--- page-overview.php
+++ page-overview.php
@@ -55,6 +55,15 @@
         </tr>
       </tbody>
     </table>
+    <div class="map">
+        <?php
+          if(have_posts()):
+            while(have_posts()) : the_post();
+             the_content();
+            endwhile;
+          endif;
+        ?>
+    </div>
   </div>
 </main>
 
